fb54ceda2957be7dfb13548d4df27b17ed42f76f
alvaroosxnchzz <alvarosanchezortega34@gmail.com>
2024-01-09 13:09:38 +0100

    Añadir valores a la clase Clientes.

---
+++ gestionhotel/src/main/java/Clientes.java
@@ -0,0 +1,58 @@
+
+public class Clientes {
+	public String nombre = "";
+	
+	public String apellido1 = "";
+	
+	public String apellido2 = "";
+	
+	public int edad = 0;
+	
+	public String DNI = "";
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public Clientes() {
+		super();
+	}
+	
+	
+}
