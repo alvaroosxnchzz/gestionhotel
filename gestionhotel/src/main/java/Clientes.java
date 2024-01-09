d25a2b64d064a3a4ad4eec07afc1ba5b39bb756b
alvaroosxnchzz <alvarosanchezortega34@gmail.com>
2024-01-09 13:19:14 +0100

    Creada las variables (Strings y demas) Clase Clientes.

--- gestionhotel/src/main/java/Clientes.java
+++ gestionhotel/src/main/java/Clientes.java
@@ -1,4 +1,69 @@
 
 public class Clientes {
+	public String nombre;
+	
+	public String apellido1;
+	
+	public String apellido2;
+	
+	public int edad = 0;
+	
+	public String DNI;
+	
+	
 
+	public Clientes(String nombre, String apellido1, String apellido2, int edad, String dNI) {
+		super();
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+		this.edad = edad;
+		DNI = dNI;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido2) {
+		this.apellido2 = apellido2;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(String dNI) {
+		DNI = dNI;
+	}
+
+	public Clientes() {
+		super();
+	}
+	
+	
 }
