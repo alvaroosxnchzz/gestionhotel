1f809b16ad1f13b1c4502c5b1d08e5ed67aed990
alvaroosxnchzz <alvarosanchezortega34@gmail.com>
2024-01-10 12:25:48 +0100

    Creacion clase Hotel.

--- gestionhotel/src/main/java/Hotel.java
+++ gestionhotel/src/main/java/Hotel.java
@@ -1,5 +1,60 @@
 
 
 public class Hotel {
+	
+	public String peticion;
+	
+	public String factura;
+	
+	public int Habitaciones;
+	
+	public String cliente;
 
+	public String getPeticion() {
+		return peticion;
+	}
+
+	public void setPeticion(String peticion) {
+		this.peticion = peticion;
+	}
+
+	public String getFactura() {
+		return factura;
+	}
+
+	public void setFactura(String factura) {
+		this.factura = factura;
+	}
+
+	public int getHabitaciones() {
+		return Habitaciones;
+	}
+
+	public void setHabitaciones(int habitaciones) {
+		Habitaciones = habitaciones;
+	}
+
+	public String getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(String cliente) {
+		this.cliente = cliente;
+	}
+
+	public Hotel(String peticion, String factura, int habitaciones, String cliente) {
+		super();
+		this.peticion = peticion;
+		this.factura = factura;
+		Habitaciones = habitaciones;
+		this.cliente = cliente;
+	}
+
+	public Hotel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
 }
