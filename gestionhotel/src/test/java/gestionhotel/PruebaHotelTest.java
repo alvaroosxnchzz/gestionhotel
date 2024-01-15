e4f9f526345a9bc397f0471d8f7a37884a9d6011
alvaroosxnchzz <alvarosanchezortega34@gmail.com>
2024-01-15 12:50:10 +0100

    [ADD] Creada primera parte PruebaHotelTest

---
+++ gestionhotel/src/test/java/gestionhotel/PruebaHotelTest.java
@@ -0,0 +1,36 @@
+package gestionhotel;
+
+import org.junit.jupiter.api.Test;
+
+public class PruebaHotelTest {
+	@Test
+	public void gettersSettersTest() {
+		Habitacion h1 = new Habitacion();
+		h1.setNumHabitacion(1);
+		h1.setNumeroCamas("Doble");
+		h1.setCategoria("Business");
+		h1.setEstado("Libre");
+		h1.setPrecio(110);
+		Habitacion h2 = new Habitacion();
+		h2.setNumHabitacion(2);
+		h2.setNumeroCamas("Individual");
+		h2.setCategoria("Normal");
+		h2.setEstado("Ocupado");
+		h2.setPrecio(35);
+		Habitacion h3 = new Habitacion();
+		h3.setNumHabitacion(3);
+		h3.setNumeroCamas("Triple");
+		h3.setCategoria("Superior");
+		h3.setEstado("No disponible");
+		h3.setPrecio(235);
+		
+		Hotel h = new Hotel();
+		h.setNombre("Hotel del Sur");
+		h.addHabitacion(h1);
+		h.addHabitacion(h2);
+		h.addHabitacion(h3);
+		h.setCategoria("Premium");
+		
+		
+	}
+}
