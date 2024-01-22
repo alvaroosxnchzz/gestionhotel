9d6900ff2192898d49dee913149c56065d33456b
alvaroosxnchzz <alvarosanchezortega34@gmail.com>
2024-01-22 10:45:12 +0100

    [ADD]  Creacion clase menu e inicio caso de uso 1

---
+++ gestionhotel/src/main/java/gestionhotel/Menu.java
@@ -0,0 +1,27 @@
+package gestionhotel;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Menu {
+
+    private static Hotel hotel;
+
+    public static void cargarHotel(String nombreHotel){
+
+
+        hotel=new Hotel(nombreHotel);
+    }
+
+    public static void cu01(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce una fecha de entrada");
+        LocalDate fechaEntrada = LocalDate.parse(sc.nextLine());
+        System.out.println("Introduce una fecha de salida");
+        LocalDate fechaSalida = LocalDate.parse(sc.nextLine());
+        System.out.println("Introduce el numero de personas");
+        int numeroPersonas = sc.nextInt();
+
+    }
+   }
