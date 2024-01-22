29ae8b8a29ec5982955bc9fd4bf7d7a85252544a
alvaroosxnchzz <alvarosanchezortega34@gmail.com>
2024-01-22 11:06:39 +0100

    [ADD] Añadido for de nuevo para devolver numero y precio de habitacion.

--- gestionhotel/src/main/java/gestionhotel/Menu.java
+++ gestionhotel/src/main/java/gestionhotel/Menu.java
@@ -23,10 +23,16 @@ public class Menu {
         LocalDate fechaSalida = LocalDate.parse(sc.nextLine());
         System.out.println("Introduce el numero de personas");
         int numeroPersonas = sc.nextInt();
+        
         BookingService bs = new BookingService(hotel);
         ArrayList<Boolean> habitacionesLibres =  bs.consultarDisponibilidad(fechaEntrada,fechaSalida,numeroPersonas);
-
-
+        
+        for (Habitacion h : hotel.getConjuntoHabitaciones()) {
+            if (habitacionesLibres.get(h.getNumHabitacion() - 1)) {
+                System.out.println(h.getNumHabitacion());
+                System.out.println(h.getPrecio());
+            }
+        }
     }
     
     public static void cu02(ArrayList<Boolean> habitacionesLibres, LocalDate fechaEntrada, LocalDate fechaSalida, int numeroHabitacion){}
