4e48d6969a47ca18f8809af6b2ac53a826749662
Adrian Candon <adriancandon00@gmail.com>
2024-02-19 09:15:22 +0100

    [DEL] Borrar metodo Consultar Disponibilidad

--- gestionhotel/src/main/java/gestionhotel/BookingService.java
+++ gestionhotel/src/main/java/gestionhotel/BookingService.java
@@ -11,26 +11,7 @@ public class BookingService {
 		this.h = h;
 	}
 
-	public ArrayList<Boolean> consultarDisponibilidad(LocalDate fechaEntrada, LocalDate fechaSalida, int numPersonas){
-		
-		ArrayList<Boolean> habitacionesLibres = new ArrayList<Boolean>(h.getConjuntoHabitaciones().size());
-		habitacionesLibres.replaceAll(true);
-		
-		for(Reservas r : h.getTotalReservas()){
-            if( !fechaEntrada.isAfter(r.getFechaSalida()) && !fechaSalida.isBefore(r.getFechaEntrada()) ){
-                habitacionesLibres.set(r.getNumHabitacion() - 1, false);
-            }
-        }
-		
-		for(Habitacion hab : h.getConjuntoHabitaciones()){
-            if( habitacionesLibres.get(hab.getNumHabitacion()-1) && (hab.getEstado().equals("No disponible") || hab.getNumeroCamas() < numPersonas) ){
-                habitacionesLibres.set(hab.getNumHabitacion()-1, false);
-            }
-        }
-
-        
-        return habitacionesLibres;
-		
+	
 		
 	
 		
