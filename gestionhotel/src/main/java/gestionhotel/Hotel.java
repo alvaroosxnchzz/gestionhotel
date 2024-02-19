ceb726bf6c6ec591bd976688898ad6ac1596d0dd
Adrian Candon <adriancandon00@gmail.com>
2024-02-19 11:10:23 +0100

    [ADD/FIX] Clase Hotel

--- gestionhotel/src/main/java/gestionhotel/Hotel.java
+++ gestionhotel/src/main/java/gestionhotel/Hotel.java
@@ -1,66 +1,54 @@
 package gestionhotel;
 import java.util.ArrayList;
-
-import gestionhotel2.HashMap;
-
-public class Hotel {
-	
-	public String nombre;
-	public ArrayList<Habitacion> conjuntoHabitaciones = new ArrayList<Habitacion>();
-	public ArrayList<Reserva> totalReservas = new ArrayList<Reserva>();
-	public String categoria;
-	
-
-	public Hotel(String nombre) {
-		this.nombre = nombre;
-	}
+import java.util.List;
 
 
-	public String getNombre() {
-		return nombre;
-	}
+public class Hotel{
+
+    private ArrayList<Habitacion> conjuntoHabitaciones;
+    private ArrayList<Cliente> conjuntoClientes;
+    private ArrayList<Reserva> conjuntoReservas;
+
+    
+    
 
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	
-	public void addHabitacion(Habitacion h) {
-		conjuntoHabitaciones.add(h);
-	}
-	
-	public void addReserva(Reserva r){
-        totalReservas.add(r);
+    private HabitacionDAOImpl hdao = new HabitacionDAOImpl();
+    private ClienteDAOImpl cdao = new ClienteDAOImpl();
+    private ReservaDAOImpl rdao = new ReservaDAOImpl();
+
+    public Hotel() {
+        // Cargada la info del hotel
+        conjuntoHabitaciones = hdao.obtenerTodo();
+        conjuntoClientes = cdao.obtenerTodo();
+        conjuntoReservas = rdao.obtenerTodo();
+
     }
 
+    public void addHabitacion(Habitacion h) {
+        conjuntoHabtaciones.add(h);
+    }
 
-	public ArrayList<Habitacion> getConjuntoHabitaciones() {
-		return conjuntoHabitaciones;
-	}
+    
+    public void addReserva(Reserva r){
+        conjuntoReservas.add(r);
+    }
 
+    public void addCliente(Cliente c){
+        conjuntoClientes.add(c);
+    }
 
-	public void setConjuntoHabitaciones(ArrayList<Habitacion> conjuntoHabitaciones) {
-		this.conjuntoHabitaciones = conjuntoHabitaciones;
-	}
+    public List<Habitacion> getConjuntoHabitaciones(){
+        return conjuntoHabitaciones;
+    }
 
+    public List<Habitacion> getConjuntoReservas(){
+        return conjuntoReservas;
+    }
 
-	public String getCategoria() {
-		return categoria;
-	}
-
-
-	public void setCategoria(String categoria) {
-		this.categoria = categoria;
-	}
-
-
-	public ArrayList<Reserva> getTotalReservas() {
-		return totalReservas;
-	}
-
-
-	public void setTotalReservas(ArrayList<Reserva> totalReservas) {
-		this.totalReservas = totalReservas;
+    public List<Habitacion> getConjuntoClientes(){
+        return conjuntoClientes;
+    }		this.totalReservas = totalReservas;
 	}
 	
 	public HashMap<String, Integer> contarHabitaciones(int numPersonas){
